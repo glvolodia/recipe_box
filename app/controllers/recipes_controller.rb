2166376f62a389b0668b3a2c0738d76a43884e9a
Volodia <glvolodia@gmail.com>
2017-03-06 19:11:17 +0200

    Added users

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,7 +6,7 @@ class RecipesController < ApplicationController
 	end
 	
 	def new
-		@recipe = Recipe.new
+		@recipe = current_user.recipes.build
 	end
 	
 	def show
@@ -16,7 +16,7 @@ class RecipesController < ApplicationController
 	end
 
 	def create
-		@recipe = Recipe.new(recipe_params)
+		@recipe = current_user.recipes.build(recipe_params)
 		if @recipe.save 
 			redirect_to @recipe, notice: "Successfully created new recipe"
 		else 
